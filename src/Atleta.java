b9c5c4e55111e16ab828ef381c1ef38d94397323
f <f>
2025-11-13 13:18:15 +0100

    codice sistemato

--- src/Atleta.java
+++ src/Atleta.java
@@ -4,38 +4,31 @@ import java.util.Random;
 public class Atleta implements Runnable {
     int numero;
     String nome;
-    Giudice giudice;
-    double tempo = (double)0.0F;
-    double metri = (double)0.0F;
-    final double LUNGHEZZAGARA = (double)50.0F;
+    //      double tempo = 0;
+    double metri = 0;
+    final double LUNGHEZZAGARA = 50.0;
 
 
-    public Atleta(int pNumero, String pNome, Giudice pgiudice) {
-        this.numero = pNumero;
-        this.giudice= pgiudice;
-        this.nome = pNome;
+    public Atleta(int pNumero, String pNome) {
+        numero = pNumero;
+        nome = pNome;
+        Giudice.aggiungimi(this);
     }
 
 
+    @Override
     public void run() {
         Random metriPercorsi = new Random();
-
-
-        while(this.metri <= (double)50.0F) {
-            this.metri += metriPercorsi.nextDouble((double)10.0F);
-            System.out.println(this.nome + " Metri Percorsi: " + this.metri);
-
-
-            try {
-                Thread.currentThread();
-                Thread.sleep(1000L);
-            } catch (InterruptedException var3) {
-                System.err.println("Errore sleep");
-            }
+        while(metri <= LUNGHEZZAGARA) {
+            metri += metriPercorsi.nextDouble(10);
+            System.out.println(nome + " Metri Percorsi: " + metri);
+            try { Thread.currentThread().sleep(1000); }
+            catch (InterruptedException e) { System.err.println("Errore sleep"); }
         }
 
 
-        System.out.println(this.nome + " è al Traguardo");
-        giudice.finito(this);
+        Giudice.finito(this);
     }
 }
+
+
--- src/GaraAtletica.java
+++ src/GaraAtletica.java
@@ -1,31 +1,15 @@
 public class GaraAtletica {
     public static void main(String[] args) {
         System.out.println("Gara Atletica");
-        Giudice giudice= new Giudice();
 
 
-        Atleta a1 = new Atleta(67, "Pacchiu",giudice);
-        Atleta a2 = new Atleta(68, "Pacchioso",giudice);
-        Thread ta2 = new Thread(a2);
-        ta2.setPriority(10);
-        Thread ta1 = new Thread(a1);
+        new Atleta(67, "babis");
+        new Atleta(68, "mumus");
+        new Atleta(69, "piro");
 
 
-        for(int i = 3; i > 0; --i) {
-            System.out.println("Inizio in:" + i);
-        }
-
-
-        ta1.start();
-
-
-        try {
-            ta1.join();
-        } catch (InterruptedException var6) {
-            System.err.println("Errore join");
-        }
-
-
-        ta2.start();
+        Giudice.avviaGara();
+        // System.out.println("Caratteristiche Main: "+ Thread.currentThread().getName()
+        // +" - " + Thread.currentThread().getPriority());
     }
 }
